47b0d570519f3a5b60bab3fdb67ea397ebb06f28
Autumn Stephens <aust8150@colorado.edu>
2025-09-01 21:41:14 -0600

    Implemented 'get_column()' function, which looks for column data that matches a user-given search condition

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,16 @@
+import numpy as np
+
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+
+    results = np.array([])
+
+    f = open(file_name, "r")
+    for l in f:
+        A = l.rstrip().split(",")
+        
+        if A[query_column]==query_value:
+            results = np.append(results, A[result_column])
+
+    f.close()
+
+    return results
